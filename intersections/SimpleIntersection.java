efb3fb394999871692f2cd5df6f1cb3cebc00354
Radu3121 <80837837+Radu3121@users.noreply.github.com>
2021-05-17 16:31:14 +0300

    Delete intersections directory

--- intersections/SimpleIntersection.java
+++
@@ -1,7 +0,0 @@
-package com.apd.tema2.intersections;
-
-import com.apd.tema2.entities.Intersection;
-
-public class SimpleIntersection implements Intersection {
-    // Define your variables here.
-}
